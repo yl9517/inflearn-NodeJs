18a5e6ff066dff8ed7f248f99ff9fe7ff96ef4d3
DESKTOP-F89735T\이여름 <yl9517@naver.com>
2021-11-28 19:48:18 +0900

    bodyParser이용하여 회원가입 기능

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express') //express 모듈 가져오기
 const app = express()           //func이용하여 새로운 express 생성
 const port = 5000               //포트번호
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://summer:abcd1234@inflearn.c7bl9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
@@ -9,6 +14,20 @@ mongoose.connect('mongodb+srv://summer:abcd1234@inflearn.c7bl9.mongodb.net/myFir
 
 app.get('/', (req, res) => { 
   res.send('Hello World!안녕하세요! 인프런테스트중!')
+})
+
+app.post('/register', (req,res) =>{
+  //회원가입할때 필요한 정보들을 client에서 가져오기 (User.js)
+  //그것들을 DB에 넣어주기
+
+  const user = new User(req.body) //bodyparser를 이용해서 클랑리언트 정보 가져옴
+
+  user.save((err, userInfo) =>{ //몽고db에서 오는 메소드 save
+    if(err) return res.json({success: false, err}) //에러가 있다면 json형식으로 실패와 메세지 전달
+    return res.status(200).json({   //성공
+      success:true
+    })
+  })
 })
 
 app.listen(port, () => {

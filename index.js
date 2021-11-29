3790dc91b06354a6ec3055fc76debd27a34d1f5c
yl9517 <yl9517@naver.com>
2021-11-29 16:54:38 +0900

    비밀번호 암호화 & id token 생성

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express') //express 모듈 가져오기
 const app = express()           //func이용하여 새로운 express 생성
 const port = 5000               //포트번호
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 const {User} = require("./models/User");
@@ -9,6 +10,7 @@ const {User} = require("./models/User");
 
 app.use(bodyParser.urlencoded({ extended:true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI)
@@ -16,14 +18,15 @@ mongoose.connect(config.mongoURI)
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => { 
-  res.send('Hello World!안녕하세요! 인프런테스트중! backend')
+  res.send('Hello World!안녕하세요! 인프런테스트중 backen')
 })
 
+//회원가입
 app.post('/register', (req,res) =>{
   //회원가입할때 필요한 정보들을 client에서 가져오기 (User.js)
   //그것들을 DB에 넣어주기
 
-  const user = new User(req.body) //bodyparser를 이용해서 클랑리언트 정보 가져옴
+  const user = new User(req.body) //bodyparser를 이용해서 클라이언트 정보 가져옴
 
   user.save((err, userInfo) =>{ //몽고db에서 오는 메소드 save
     if(err) return res.json({success: false, err}) //에러가 있다면 json형식으로 실패와 메세지 전달
@@ -32,6 +35,43 @@ app.post('/register', (req,res) =>{
     })
   })
 })
+
+//로그인
+app.post('/login',(req,res)=>{
+  //1. 요청 된 이메일이 DB에서 있는지 찾기
+  User.findOne({email:req.body.email}, (err,user)=>{
+    if(!user){ //해당 유저가 없다면
+      return res.json({
+        loginSuccess: false,
+        message : "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+    //2. 1번이 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) =>{
+      //user.js의 comparePassword 갔다옴
+
+      if(!isMatch)
+      return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다."
+      })
+
+
+      //3. 2번까지 같다면 Token 생성
+      user.generateToken((err,user) => {
+        if(err) return res.status(400).send(err);
+        
+        //토큰을 (쿠키에) 저장 (쿠키 외 다른 곳에도 저장 가능)
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({loginSuccess:true, userId: user._id })
+      })
+    })
+
+  })
+})
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)  //해당포트번호에서 이 app을 실행함
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken');
+
 const userSchema = mongoose.Schema({
     name:{
         type: String,
@@ -29,6 +33,47 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save',function(next){    //index.js에서 회원정보를 저장하기 전에 일어나는 동작
+  
+  let user = this;
+  
+  if(user.isModified('password')){    //비밀번호 암호화 = 사용자 정보 중 비밀번호를 바꿀때만!!
+    bcrypt.genSalt(saltRounds, function(err, salt){ 
+        if(err) return next(err)                    //에러가 났다면 에러 전달
+
+        bcrypt.hash(user.password , salt, function(err,hash){ //사용자 비밀번호, salt,  (hash = 암호화된 비밀번호)
+            if(err) return next(err)
+            user.password= hash  //암호화 성공 시 사용자 비밀번호를 hash로 변경
+            next()
+        })
+    })
+  } else{   //비밀번호 이 외 변경
+      next()
+  }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword 1234567  //db에 저장된 암호화비밀번호 $2bafdsa~~~ 
+    //입력한 pw도 암호화하여 db비밀번호와 비교
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch) //에러는 없고, true 전달
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+
+    let user = this;
+    //jsonwebtoken 이용하여 토큰 생성
+    let token = jwt.sign(user._id.toHexString(), 'anythingToken')   //user._id + 'anythingTken' = token
+
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err) //에러 
+        cb(null,user) //token 생성하여 user에 담기 성공
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
